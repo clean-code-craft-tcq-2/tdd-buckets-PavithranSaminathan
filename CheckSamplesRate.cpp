c0171f41d41b7782e8a2df3624e92e00846d0a4f
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-03-07 21:20:57 +0530

    Update CheckSamplesRate.cpp

--- CheckSamplesRate.cpp
+++ CheckSamplesRate.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "catch.hpp"
-#include "typewise-alert.h"
+#include "CheckSamplesRate.h"
 #include <stdio.h>
 
 TEST_CASE("Read No of samples from input range")

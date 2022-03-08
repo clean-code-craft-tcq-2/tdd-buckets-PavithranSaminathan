17b3dc32d434aa021ae9fb1f14f03b93a2f9abf8
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-03-08 21:57:14 +0530

    Update CheckSamplesRate.c

--- CheckSamplesRate.c
+++ CheckSamplesRate.c
@@ -1,9 +1,11 @@
 #include "CheckSamplesRate.h"
+#include <stdio.h>
 //const int CurrentSamples[7] = {3, 3, 5, 4, 10, 11, 12};
 int ReadNoofSamples(int FromRange , int ToRange,int CurrentSamples[])
 {
   int TotalSamples=0;
   int TotalCurrentSamples = sizeof(CurrentSamples)/CurrentSamples[0];
+   printf("TotalCurrentSamples= %d\n",TotalCurrentSamples);
   for(int i=0;i<TotalCurrentSamples;i++)
   {
     if(FromRange <= CurrentSamples[i] && ToRange >= CurrentSamples[i])
@@ -11,5 +13,6 @@ int ReadNoofSamples(int FromRange , int ToRange,int CurrentSamples[])
       TotalSamples++;
     }
   }
+  printf("Total Samples= %d",TotalSamples);
   return TotalSamples;
 }

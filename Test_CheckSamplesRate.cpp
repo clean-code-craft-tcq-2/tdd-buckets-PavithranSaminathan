59be772a0aba1ca24041bef532664c9a106fbab3
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-03-08 23:16:40 +0530

    Update Test_CheckSamplesRate.cpp

--- Test_CheckSamplesRate.cpp
+++ Test_CheckSamplesRate.cpp
@@ -3,17 +3,5 @@
 #include "CheckSamplesRate.h"
 TEST_CASE("Read No of samples from input range")
 {
-  //int Total_samples=0;
-  int FromRange=5;
-  int ToRange=10;
- // Total_samples=ReadNoofSamples(FromRange,ToRange);
-  REQUIRE(ReadNoofSamples(FromRange,ToRange) == 2);
+ REQUIRE(ReadNoofSamples(5,10) == 2);
 }
-//TEST_CASE("Read No of samples from 2-6 range")
-//{
-//  int Total_samples=0;
-//  int FromRange=2;
-//  int ToRange=6;
-//  Total_samples=ReadNoofSamples(FromRange,ToRange);
-//  REQUIRE(Total_samples == 4);
-//}

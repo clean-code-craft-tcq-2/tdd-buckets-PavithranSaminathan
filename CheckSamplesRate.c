a7d349c592591ab193d433980c92199c4655f343
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-03-08 21:30:37 +0530

    Update CheckSamplesRate.c

--- CheckSamplesRate.c
+++ CheckSamplesRate.c
@@ -1,6 +1,6 @@
 #include "CheckSamplesRate.h"
-const int CurrentSamples[7] = {3, 3, 5, 4, 10, 11, 12};
-int ReadNoofSamples(int FromRange , int ToRange)
+//const int CurrentSamples[7] = {3, 3, 5, 4, 10, 11, 12};
+int ReadNoofSamples(int FromRange , int ToRange,const int *CurrentSamples)
 {
   int TotalSamples=0;
   int TotalCurrentSamples = sizeof(CurrentSamples)/CurrentSamples[0];

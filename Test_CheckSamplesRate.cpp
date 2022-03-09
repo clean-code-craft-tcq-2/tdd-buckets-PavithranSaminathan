be13ca3b60de7e5dda4b11a06da6b6e2d602ddb6
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-03-09 08:49:00 +0530

    Update Test_CheckSamplesRate.cpp

--- Test_CheckSamplesRate.cpp
+++ Test_CheckSamplesRate.cpp
@@ -4,29 +4,24 @@
 #include "CheckSamplesRate.h"
 #include <stdio.h>
 
-TEST_CASE("Read No of samples from 5-10 range")
-{
  int TotalNoOfSamples=0;
  int CurrentSamples[7] = {3, 3, 5, 4, 10, 11, 12};
  int TotalSamples=sizeof(CurrentSamples)/sizeof(CurrentSamples[0]);
+
+TEST_CASE("Read No of samples from 5-10 range")
+{
  TotalNoOfSamples=ReadNoofSamples(CurrentSamples,TotalSamples,5,10);
  REQUIRE(TotalNoOfSamples == 2);
 }
 
 TEST_CASE("Read No of samples from 3-5 range")
 {
- int TotalNoOfSamples=0;
- int CurrentSamples[7] = {3, 3, 5, 4, 10, 11, 12};
- int TotalSamples=sizeof(CurrentSamples)/sizeof(CurrentSamples[0]);
  TotalNoOfSamples=ReadNoofSamples(CurrentSamples,TotalSamples,3,5);
  REQUIRE(TotalNoOfSamples == 4);
 }
 
 TEST_CASE("Read No of samples from 3-12 range")
 {
- int TotalNoOfSamples=0;
- int CurrentSamples[7] = {3, 3, 5, 4, 10, 11, 12};
- int TotalSamples=sizeof(CurrentSamples)/sizeof(CurrentSamples[0]);
- TotalNoOfSamples=ReadNoofSamples(CurrentSamples,TotalSamples,3,5);
+ TotalNoOfSamples=ReadNoofSamples(CurrentSamples,TotalSamples,3,12);
  REQUIRE(TotalNoOfSamples == 7);
 }

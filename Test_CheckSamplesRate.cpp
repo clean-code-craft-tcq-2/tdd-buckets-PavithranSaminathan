2ed564bc651b68cde6e39b1838d6f308fe189f33
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-03-09 11:30:44 +0530

    Update Test_CheckSamplesRate.cpp

--- Test_CheckSamplesRate.cpp
+++ Test_CheckSamplesRate.cpp
@@ -4,20 +4,37 @@
 #include "CheckSamplesRate.h"
 #include <stdio.h>
 
+int TotalNoOfSamples=0;
+#define INVALID_RANGE 0xFF
+
+/*Reading occurance betweeen 5-10 range */
 TEST_CASE("Read No of samples from 5-10 range")
 {
  TotalNoOfSamples=ReadNoofSamples(5,10);
  REQUIRE(TotalNoOfSamples == 2);
 }
 
+/*Reading occurance betweeen invalid range range */
+/*Function should return invalid range*/
+TEST_CASE("Test case invalid range")
+{
+ TotalNoOfSamples=ReadNoofSamples(12,3);
+ REQUIRE(TotalNoOfSamples == INVALID_RANGE);
+}
+
+/*Reading occurance betweeen 3-5 range */
 TEST_CASE("Read No of samples from 3-5 range")
 {
  TotalNoOfSamples=ReadNoofSamples(3,5);
  REQUIRE(TotalNoOfSamples == 4);
 }
 
+/*Reading occurance betweeen 3-12 range */
 TEST_CASE("Read No of samples from 3-12 range")
 {
  TotalNoOfSamples=ReadNoofSamples(3,12);
  REQUIRE(TotalNoOfSamples == 7);
 }
+
+
+

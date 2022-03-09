0bcacf07315ac4e4f4ea0d4022daba6ba231721e
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-03-09 11:22:37 +0530

    Update Test_CheckSamplesRate.cpp

--- Test_CheckSamplesRate.cpp
+++ Test_CheckSamplesRate.cpp
@@ -4,10 +4,6 @@
 #include "CheckSamplesRate.h"
 #include <stdio.h>
 
- int TotalNoOfSamples=0;
- int CurrentSamples[7] = {3, 3, 5, 4, 10, 11, 12};
- int TotalSamples=sizeof(CurrentSamples)/sizeof(CurrentSamples[0]);
-
 TEST_CASE("Read No of samples from 5-10 range")
 {
  TotalNoOfSamples=ReadNoofSamples(CurrentSamples,TotalSamples,5,10);

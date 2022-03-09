c1032ee8bbc4531e2a57563f3108ec9cb0e1b6ee
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-03-09 08:40:07 +0530

    Update Test_CheckSamplesRate.cpp

--- Test_CheckSamplesRate.cpp
+++ Test_CheckSamplesRate.cpp
@@ -8,7 +8,7 @@ TEST_CASE("Read No of samples from input range")
 {
  
  int TotalNoOfSamples=0;
- const int CurrentSamples[7] = {3, 3, 5, 4, 10, 11, 12};
+ int CurrentSamples[7] = {3, 3, 5, 4, 10, 11, 12};
  int TotalSamples=sizeof(CurrentSamples)/sizeof(CurrentSamples[0]);
  
  TotalNoOfSamples=ReadNoofSamples(CurrentSamples,TotalSamples,5,10);

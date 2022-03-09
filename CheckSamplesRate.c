7f9b5adcc3f272c9aa65c2f5757d427115a584fd
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-03-09 11:23:30 +0530

    Update CheckSamplesRate.c

--- CheckSamplesRate.c
+++ CheckSamplesRate.c
@@ -1,6 +1,10 @@
 #include "CheckSamplesRate.h"
 #include <stdio.h>
 
+int TotalNoOfSamples=0;
+int CurrentSamples[7] = {3, 3, 5, 4, 10, 11, 12};
+int TotalSamples=sizeof(CurrentSamples)/sizeof(CurrentSamples[0]);
+
 void PrintonConsole(char * InputData)
 {
   printf("%s",InputData);

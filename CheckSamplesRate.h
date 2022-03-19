d6f35a808111bfab34a5946c4eb806e66e7c6ade
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-03-19 19:27:15 +0530

    Update CheckSamplesRate.h

--- CheckSamplesRate.h
+++ CheckSamplesRate.h
@@ -1,7 +1,4 @@
 #ifndef CheckSamplesRate_h
 #define CheckSamplesRate_h
-
-
-char* ReadNoofSamples(int FromRange, int ToRange);
-
+char* ReadNoofSamples(int FromRange , int ToRange , char *Output);
 #endif

c28201051de6ed6dca91b3adc87c8b4abda37bde
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-03-19 22:14:29 +0530

    Update Test_CheckSamplesRate.cpp

--- Test_CheckSamplesRate.cpp
+++ Test_CheckSamplesRate.cpp
@@ -11,7 +11,7 @@ int TotalNoOfSamples=0;
 /*Reading occurance betweeen 5-10 range */
 TEST_CASE("Read No of samples from 5-10 range")
 {
- char Output[30];
+ char Output[30]={'\0'};
  int Min_Range=5;
  int Max_Range=10;
   

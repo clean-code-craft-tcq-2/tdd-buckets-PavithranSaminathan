ad4199dc74bc71b21b93550844bdf0ac7f600e53
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-03-20 12:14:31 +0530

    Update Test_CheckSamplesRate.cpp

--- Test_CheckSamplesRate.cpp
+++ Test_CheckSamplesRate.cpp
@@ -8,31 +8,34 @@
 int TotalNoOfSamples=0;
 #define INVALID_RANGE 0xFF
 
+char Actual_Output[30]={'\0'};
+
 /*Reading occurance betweeen 5-10 range */
 TEST_CASE("Read No of samples from 5-10 range")
 {
- char Output[30]={'\0'};
  char Expected_Output[30]="5-10,2";
  int Min_Range=5;
  int Max_Range=10;
-  
- ReadNoofSamples(Min_Range,Max_Range,Output);
- REQUIRE(strcmp(Expected_Output,Output) == 0);
+ ReadNoofSamples(Min_Range,Max_Range,Actual_Output);
+ REQUIRE(strcmp(Expected_Output,Actual_Output) == 0);
 }
 
 /*Reading occurance betweeen invalid range range */
 /*Function should return invalid range*/
 TEST_CASE("Test case invalid range")
 {
- TotalNoOfSamples=ReadNoofSamples(12,3);
- REQUIRE(TotalNoOfSamples == INVALID_RANGE);
+ char Expected_Output[30]="INVALID_OUTPUT";
+ ReadNoofSamples(12,3,Output);
+ REQUIRE(strcmp(Expected_Output,Actual_Output) == 0);
 }
+
 
 /*Reading occurance betweeen 3-5 range */
 TEST_CASE("Read No of samples from 3-5 range")
 {
- TotalNoOfSamples=ReadNoofSamples(3,5);
- REQUIRE(TotalNoOfSamples == 4);
+ char Expected_Output[30]="3-5,4";
+ TotalNoOfSamples=ReadNoofSamples(3,5,Actual_Output);
+ REQUIRE(strcmp(Expected_Output,Actual_Output) == 0);
 }
 
 /*Reading occurance betweeen 3-12 range */

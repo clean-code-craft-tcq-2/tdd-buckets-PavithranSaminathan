37d22d8ffef1ad4377fed923796b497e359ffe10
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-03-20 13:57:16 +0530

    Update Test_CheckSamplesRate.cpp

--- Test_CheckSamplesRate.cpp
+++ Test_CheckSamplesRate.cpp
@@ -47,5 +47,21 @@ TEST_CASE("Read No of samples from 3-12 range")
  REQUIRE(strcmp(Expected_Output,Actual_Output) == 0);
 }
 
+/*Reading occurance betweeen 2-12 range */
+TEST_CASE("Read No of samples from 2-12 range")
+{
+ char Expected_Output[30];
+ ReadNoofSamples(2,12,Actual_Output);
+ strcpy(Expected_Output,Actual_Output);
+ REQUIRE(strcmp("INVALID_RANGE" ,Expected_Output) == 0);
+}
 
+/*Reading occurance betweeen 3-13 range */
+TEST_CASE("Read No of samples from 3-13 range")
+{
+ char Expected_Output[30];
+ ReadNoofSamples(3,13,Actual_Output);
+ strcpy(Expected_Output,Actual_Output);
+ REQUIRE(strcmp("INVALID_RANGE" ,Expected_Output) == 0);
+}
 

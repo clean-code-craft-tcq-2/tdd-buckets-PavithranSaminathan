9bd9f9f6011b0a83b2b54e0e17d56e5a8f227514
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-03-20 13:25:52 +0530

    Update CheckSamplesRate.c

--- CheckSamplesRate.c
+++ CheckSamplesRate.c
@@ -7,12 +7,12 @@
 int CurrentSamples[7] = {3, 3, 5, 4, 10, 11, 12};
 int TotalSamples=sizeof(CurrentSamples)/sizeof(CurrentSamples[0]);
 
-void PrintonConsole(char * InputData)
+void PrintonConsole(char InputData [])
 {
   printf("%s",InputData);
 }
 
-char* SendDataToPrint(int FromRange , int ToRange ,int TotalOccurance,char *PrintData )
+char* SendDataToPrint(int FromRange , int ToRange ,int TotalOccurance,char PrintData[] )
 {
   sprintf(PrintData,"%d-%d,%d",FromRange,ToRange,TotalOccurance);
   PrintonConsole(PrintData);

46ff1390326fb89c7ba7e7b4e5d163cdd8758b03
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-03-27 22:40:21 +0530

    Create ADC_TEST.c

---
+++ ADC_TEST.c
@@ -0,0 +1,15 @@
+#include "ADC_TEST.h"
+
+int* CalculationADCValue(int * InputSensorValue,int size)
+{
+  int *ActualADCValue;
+  int calculateADCValue;
+  
+  for(int index=0; size>index ; index++)
+  {
+     calculateADCValue =  ((10 / 4094) * InputSensorValue[index]) + 0.5;
+     ActualADCValue[index]=calculateADCValue;
+  }
+  
+  return ActualADCValue;
+}

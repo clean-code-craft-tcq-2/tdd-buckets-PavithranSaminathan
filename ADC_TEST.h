5637b9184e7db1a30dcb6995e6e7b9e8693b6471
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-03-28 08:59:19 +0530

    Update ADC_TEST.h

--- ADC_TEST.h
+++ ADC_TEST.h
@@ -1,6 +1,6 @@
 #ifndef ADC_TEST_H
 #define ADC_TEST_H
 
-int* CalculationADCValue(int * InputSensorValue , int size);
+int* CalculationADCValue(int * InputSensorValue, int *ActualADCValue ,int size)
 
 #endif

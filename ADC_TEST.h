ad319ad463a821ab7c1651134818c9614421f47c
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-03-28 22:22:32 +0530

    Update ADC_TEST.h

--- ADC_TEST.h
+++ ADC_TEST.h
@@ -2,5 +2,6 @@
 #define ADC_TEST_H
 
 int* CalculationADCValue(int * InputSensorValue, int *ActualADCValue ,int size);
+void AnalogConvertToCurrentMagnitude(int * SensorValue ,int * ActualOutput ,int size);
 
 #endif

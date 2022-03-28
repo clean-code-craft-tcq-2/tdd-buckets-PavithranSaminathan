874fbde4d167775490b69a5946a3193637eecb4c
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-03-28 19:09:30 +0530

    Update ADC_TEST.c

--- ADC_TEST.c
+++ ADC_TEST.c
@@ -1,20 +1,14 @@
 #include "ADC_TEST.h"
 #include <stdlib.h>
-#include  <stdio.h>
 #include <float.h>
 
 int* CalculationADCValue(int * InputSensorValue, int *ActualADCValue ,int size)
 {
-  
   float calculateADCValue;
-  
   for(int index=0; size > index ; index++)
   {
     calculateADCValue =  (((float)10* (float)InputSensorValue[index]) / (float)4094 )+ (float)0.5;
     ActualADCValue[index]= (int)calculateADCValue;
-    printf("\n %f   ",calculateADCValue);
-     printf("%d %d\n",InputSensorValue[index] ,ActualADCValue[index]);
   }
-  
   return ActualADCValue;
 }

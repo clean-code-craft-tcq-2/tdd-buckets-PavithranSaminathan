13f65526fbd9ddced58391ef68c5f1e80791fab3
PavithranSaminathan <90909245+PavithranSaminathan@users.noreply.github.com>
2022-03-28 22:30:25 +0530

    Update ADC_TEST.c

--- ADC_TEST.c
+++ ADC_TEST.c
@@ -14,14 +14,13 @@ int* CalculationADCValue(int * InputSensorValue, int *ActualADCValue ,int size)
 }
 
 
-void AnalogConvertToCurrentMagnitude(int * SensorValue ,int * *MagnitudeOutput ,int size)
+void AnalogConvertToCurrentMagnitude(int * SensorValue ,int *MagnitudeOutput ,int size)
 {
   int Max_Magnitude=30;
-  int Min_Magnitude =0;
   float OutputMagnitudeValue=0;
- for(int index=0; size > index ; index++)
-  {
-     OutputMagnitudeValue= ((((float)(Max_Magnitude/1023))*(float)(SensorValue[index])) -(Max_Magnitude/2));
-     ActualOutput = (int)OutputMagnitudeValue;
-  }
+   for(int index=0; size > index ; index++)
+    {
+       OutputMagnitudeValue= ((((float)(Max_Magnitude/1023))*(float)(SensorValue[index])) -(Max_Magnitude/2));
+      MagnitudeOutput[index] = (int)OutputMagnitudeValue;
+    }
 }
